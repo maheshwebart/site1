973b7ecbe9544292bc4550db44bffd784870b831
maheshwebart <48667860+maheshwebart@users.noreply.github.com>
2023-12-22 19:19:03 +0530

    Adding scroll animation
    Adding scroll animation to the stats section

--- main.js
+++ main.js
@@ -11,3 +11,20 @@ function showMenu() {
 }
 
 // }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+    console.log(entry)
+    if(entry.isIntersecting) {
+    entry.target.classList.add('show');
+    } else {
+    entry.target.classList.remove('show');
+    
+    }
+    
+    });
+    
+    });
+    
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
